54c59b6d4c95ce098a8614abee5315f6081ea4b1
Jeffgmw <108535585+Jeffgmw@users.noreply.github.com>
2024-08-28 13:38:34 +0300

    structure and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    id ("kotlin-kapt")
+//    id ("dagger.hilt.android.plugin")
+//    id ("kotlin-parcelize")
+    id ("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -45,4 +50,36 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+    implementation ("androidx.core:core-ktx:1.13.1")
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation ("com.airbnb.android:lottie:3.7.2")
+    implementation ("io.coil-kt:coil:1.3.0")
+    implementation ("org.jsoup:jsoup:1.13.1")
+
+    implementation ("com.google.code.gson:gson:2.8.7")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation ("com.google.dagger:hilt-android:2.51")
+    kapt ("com.google.dagger:hilt-android-compiler:2.51")
+
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
 }
---
+++ app/src/main/java/com/example/somalearn/SomaLearnApplication.kt
@@ -0,0 +1,4 @@
+package com.example.somalearn
+
+class SomaLearnApplication {
+}
